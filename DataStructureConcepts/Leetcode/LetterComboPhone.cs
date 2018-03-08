10fcebb357c8101db2f787f3f8c37e8631530296
Sanjay Bhat <SanyTiger@users.noreply.github.com>
2018-03-08 03:11:37 -0600

    Added solutions to problem numbered 17, 564, 19 and BestPathAcrossMatrix problem
    Added solutions to problem numbered 17, 564, 19 and BestPathAcrossMatrix problem

--- DataStructureConcepts/Leetcode/LetterComboPhone.cs
+++ DataStructureConcepts/Leetcode/LetterComboPhone.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-/*
-* Test Cases: 12 / 25
-*/
+using System.Collections;
+
 namespace Leetcode
 {
     [TestClass]
@@ -12,69 +11,29 @@ namespace Leetcode
         [TestMethod]
         public void TestMethod1()
         {
-            var sol = LetterComboPhoneSolution.LetterCombinations("2");
+            var sol = new LetterComboPhoneSolution().LetterCombinations("2345");
         }
     }
-    public static class LetterComboPhoneSolution
+
+    public class LetterComboPhoneSolution
     {
-        public static IList<string> LetterCombinations(string digits)
+        public IList<string> LetterCombinations(string digits)
         {
-            IList<string> lstLetter = new List<string>();
-            var arrLetter = new string[10];
-            for (int i = 0; i < digits.Length; i++)
-            {
-                char temp = digits[i];
-                arrLetter[i] = GetLetter(temp - 48);
-            }
-            
-            if (digits.Length == 1)
-            {
-                var letter = arrLetter[0];
-                for (int i = 0; i < letter.Length; i++)
-                    lstLetter.Add(string.Concat(letter[i], ""));
-            }
-            else
-            {
-                var initial = 0;
-                var increment = initial + 1;
-                while (arrLetter[increment] != null)
-                {
-                    var letter = arrLetter[initial];
-                    var secondLetter = arrLetter[increment];
-
-                    for (int j = 0; j < letter.Length; j++)
-                    {
-                        for (int k = 0; k < secondLetter.Length; k++)
-                            lstLetter.Add(string.Concat(letter[j], secondLetter[k]));
-                    }
-                    if (increment < arrLetter.Length)
-                        increment++;
-                    else if (initial < arrLetter.Length)
-                    {
-                        initial++;
-                        increment = initial + 1;
-                    }
-                    else
-                        break;
-                }
-            }
-
-            return lstLetter;
+            var lstCombo = new List<String>();
+            var keyboard = new String[] { string.Empty, string.Empty, "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            LetterCombo(keyboard, lstCombo, digits, string.Empty);
+            return lstCombo;
         }
-        public static string GetLetter(int num)
-        {
-            switch (num)
+        public void LetterCombo(String[] keyboard, List<String> lstCombo, String digits, String str)
+          {
+            if (str.Length == digits.Length)
             {
-                case 2: return "abc";
-                case 3: return "def";
-                case 4: return "ghi";
-                case 5: return "jkl";
-                case 6: return "mno";
-                case 7: return "pqrs";
-                case 8: return "tuv";
-                case 9: return "wxyz";
-                default: return string.Empty;
+                lstCombo.Add(str);
+                return;
             }
+            var letters = keyboard[digits[str.Length] - '0'];
+            for (int i = 0; i < letters.Length; ++i)
+                LetterCombo(keyboard, lstCombo, digits, str + letters[i]);
         }
     }
 }
---
+++ DataStructureConcepts/Leetcode/RemoveNthFromEnd.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode
+{
+    // Test Cases: 194/208
+
+    [TestClass]
+    public class RemoveNthFromEnd
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var node = new ListNode(1);
+            node.next = new ListNode(2);
+
+            //node.next.next = new ListNode(3);
+            //node.next.next.next = new ListNode(4);
+            //node.next.next.next.next = new ListNode(5);
+            var sol = new RemoveNthFromEndSolution().RemoveNthFromEnd(node, 1);
+        }
+    }
+    public class RemoveNthFromEndSolution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var sub = new ListNode(0);
+            var s = sub;
+            var copyHead = head;
+            var len = 0;
+
+            while (copyHead != null)
+            {
+                ++len;
+                copyHead = copyHead.next;
+            }
+
+            if (n == 1 && len == 1)
+                return sub.next;
+            if (n == 1 && len == 2)
+                return new ListNode(head.val);
+
+            var diff = len - n;
+            len = 0;
+            while (head != null)
+            {
+                if (diff == len)
+                {
+                    ++len;
+                    head = head.next;
+                    continue;
+                }
+                else
+                    ++len;
+                s.next = head;
+                s = s.next;
+                head = head.next;
+            }
+            return sub.next;
+        }
+    }
+}
